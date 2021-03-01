b0fcfed574ce5ef68f2ac2257893f68483b4ec8f
Marisa Nesmith <marisanesmith@Marisas-iMac.local>
2021-03-01 12:32:24 -0500

    forgot to save

--- server.js
+++ server.js
@@ -2,15 +2,14 @@
 // Series of npm packages that we will use to give our server useful functionality
 
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const fs = require('fs');
+const noteTaker = require('../public/assets/js/index')
 
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
 
-// Tells node that we are creating an "express" server
+
 const app = express();
 
-// Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
@@ -25,9 +24,30 @@ app.use(express.static('public'));
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
+// app.get for the html files 
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+})
+
+// app.get for the api files
+
+app.get('/api/notes', (req, res) => res.json(noteTaker));
+
+// app.post for the api files
+
+app.post('/api/notes', (req, res) => {
+    const addNote = req.body;
+    // res.json(path.join(__dirname, 'public/notes.html'))
+});
+
 // LISTENER
 // The below code effectively "starts" our server
 
 app.listen(PORT, () => {
-  console.log(`App listening on PORT: ${PORT}`);
+console.log(`App listening on PORT: ${PORT}`);
 });
