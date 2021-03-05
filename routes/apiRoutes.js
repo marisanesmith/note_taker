3b529dc6d694dd732f7ec9b7fde3efe72673099d
marisanesmith <marisa@thisworldtraveled.com>
2021-03-04 19:28:29 -0500

    added uuid npm package

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,32 @@
 // Dependencies
 
-const path = require('path')
-// const noteTaker = require('../public/assets/js/index')
-// const express = require('express')
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const db = require('../db/db.json')
 
 // Routes
 
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.json());
-};
+    app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "../db/db.json"))
+});
 
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    id = uuidv4();
+    // req.body.it = uuidv4();
+    db.push(newNote);
+    console.log(db);
+
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(db), function(err) {
+        if (err) throw err;
+        res.json('response')
+    })
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    
+})
+
+};
--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 // DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
 
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-// const noteTaker = require('../public/assets/js/index')
-
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -16,14 +13,11 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
 // LISTENER
-// The below code effectively "starts" our server
 
 app.listen(PORT, () => {
 console.log(`App listening on PORT: ${PORT}`);
